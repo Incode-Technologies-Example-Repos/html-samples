e3e984f662fddcd4c95c164f0b4b41d56fa966b0
Levhita <armando.arias@incode.com>
2024-02-21 21:12:48 -0600

    added finished status with polling and waiting

---
+++ html-redirect-js/finished.js
@@ -0,0 +1,42 @@
+import './style.css'
+async function fetchOnboardingStatus(interviewId) {
+  const tokenServerURL=import.meta.env.VITE_TOKEN_SERVER_URL;
+  const response = await fetch(
+    `${tokenServerURL}/is-onboarding-finished?interviewId=${interviewId}`,
+    {},
+    );
+    
+    const data = await response.json();
+    return data
+  }
+  
+  async function app() {
+    const app = document.getElementById('app');
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const interviewId = urlParams.get('interviewId');
+    const flowId = urlParams.get('flowId');
+
+    app.innerHTML = `Waiting for the Onboarding status for interviewId: ${interviewId}`;
+
+    try {
+      const interval = setInterval(async ()=>{
+        const {success, finished, error } = await fetchOnboardingStatus(interviewId);
+        if (success===true && finished===true){
+          clearInterval(interval);
+          app.innerHTML =`Onboarding Finished!`;
+        } else if(success===false){
+          clearInterval(interval);
+          app.innerHTML =`There was an error: ${error}`;
+        }
+      }, 1000);
+      
+      const onboardingIsFinished = await fetchOnboardingStatus();
+      
+      app.innerHTML =`Waiting for onboarding to finish!`;
+    } catch(e) {
+      app.innerHTML = e.message;
+    } 
+  }
+  
+  document.addEventListener("DOMContentLoaded", app);
