62ecfe04be415fbbde78801f93171611e826a0ce
rafaelCamaraIncode <rafael.camara@incode.com>
2024-04-04 11:16:28 -0300

    Update finished.js
    Retrieving the token from the localStorage and passing it through the fetchScore call

--- html-redirect-js/src/finished.js
+++ html-redirect-js/src/finished.js
@@ -5,11 +5,12 @@ async function app() {
   app.innerHTML = `<h1>Loading...</h1>`;
   
   const interviewId = localStorage.getItem('interviewId');
-  if (!interviewId) {
+   if (!interviewId) {
     app.innerHTML = `<h1>Error: Invalid interviewId</h1>`;
   }
+  const token = localStorage.getItem('token');
 
-  const score = await fetchScore(interviewId);
+  const score = await fetchScore(interviewId, token);
   
   if(score.success){
     app.innerHTML =`<h1>Onboarding finished with score: ${score.score}</h1>`;
