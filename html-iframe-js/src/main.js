ce0b03758827f991af39f37ebe8b72b208c6342d
rafaelCamaraIncode <rafael.camara@incode.com>
2024-04-05 16:48:53 -0300

    Adding polling schema instructions to the Readme.md
    A

--- html-iframe-js/src/main.js
+++ html-iframe-js/src/main.js
@@ -44,7 +44,7 @@ async function app() {
         clearInterval(interval);
         app.innerHTML =`<h1>There was an error: ${onboarding.error}</h1>`;
       }
-    }, 1000);
+    }, 100000);
   } catch(e) {
     app.innerHTML =`<h1>There was an error: ${e.message}</h1>`;
   } 
